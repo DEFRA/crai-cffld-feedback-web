0d6262dfdc9751b1316a11122f40c5710bb2c147
sfitz42 <87677173+sfitz42@users.noreply.github.com>
2024-06-07 09:46:21 +0100

    Add S3 client

--- src/server/home/controller.js
+++ src/server/home/controller.js
@@ -1,9 +1,18 @@
+import { ListBucketsCommand } from "@aws-sdk/client-s3"
+import { buildS3Client } from "../common/helpers/s3-client"
+
 /**
  * A GDS styled example home page controller.
  * Provided as an example, remove or modify as required.
  */
 const homeController = {
-  handler: (request, h) => {
+  handler: async (request, h) => {
+    const s3 = buildS3Client()
+
+    const buckets = await s3.send(new ListBucketsCommand({}))
+
+    console.log(buckets)
+
     return h.view('home/index', {
       pageTitle: 'Home',
       heading: 'Home'
