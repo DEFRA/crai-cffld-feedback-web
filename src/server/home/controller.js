7f5aba4fbe3536169d37e5c6aed1732c624b3457
Sebastian <sebastian.horon95@gmail.com>
2024-06-07 14:51:48 +0100

    Remove console logs

--- src/server/home/controller.js
+++ src/server/home/controller.js
@@ -1,18 +1,9 @@
-import { ListBucketsCommand } from "@aws-sdk/client-s3"
-import { buildS3Client } from "../common/helpers/s3-client"
-
 /**
  * A GDS styled example home page controller.
  * Provided as an example, remove or modify as required.
  */
 const homeController = {
   handler: async (request, h) => {
-    const s3 = buildS3Client()
-
-    const buckets = await s3.send(new ListBucketsCommand({}))
-
-    console.log(buckets)
-
     return h.view('home/index', {
       pageTitle: 'Home',
       heading: 'Home'
